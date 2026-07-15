ea008452bd644c125b5a6c2c4eab84cc369ec936
Vishvajeet Singh <vishu2005.singh@gmail.com>
2026-07-15 20:21:52 +0530

    Refactor Loan checker using functions

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+def check_eligibility(age, salary, credit_score):
+    reasons = []
+
+    if age < 21:
+        reasons.append("Age should be at least 21 years.")
+
+    if salary < 30000:
+        reasons.append("Monthly salary should be at least 30000.")
+
+    if credit_score < 700:
+        reasons.append("Credit score should be at least 700.")
+
+    return reasons
+
+
 print("Loan Eligibility Checker")
 
 name = input("Enter Customer Name: ")
@@ -5,25 +20,21 @@ age = int(input("Enter Age: "))
 salary = float(input("Enter Monthly Salary: "))
 credit_score = int(input("Enter Credit Score: "))
 
+reasons = check_eligibility(age, salary, credit_score)
+
 print("\nLoan Report")
-print("-")
+print("----------------------------")
 print("Customer Name :", name)
 print("Age           :", age)
 print("Salary        :", salary)
 print("Credit Score  :", credit_score)
 
-if age >= 21 and salary >= 30000 and credit_score >= 700:
+if len(reasons) == 0:
     print("\nLoan Status : APPROVED")
     print("Reason : Customer meets all eligibility criteria.")
 else:
     print("\nLoan Status : REJECTED")
     print("Reason :")
 
-    if age < 21:
-        print("- Age should be at least 21 years.")
-
-    if salary < 30000:
-        print("- Monthly salary should be at least 30000.")
-
-    if credit_score < 700:
-        print("- Credit score should be at least 700.")
+    for reason in reasons:
+        print("-", reason)
